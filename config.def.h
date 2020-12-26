6c48b954f367a6374b70681e1ddd2c911d5b02df
pro7onmatrix <yannick.lemke@posteo.de>
2020-12-26 14:57:11 +0100

    Add poweroff script

--- config.def.h
+++ config.def.h
@@ -31,6 +31,8 @@ static const Rule rules[] = {
   /* class      instance    title       tags mask     isfloating   monitor */
   { "Gimp",     NULL,       NULL,       0,            1,           -1 },
   { "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+  { "Thunderbird", NULL,    NULL,       1 << 2,       0,           -1 },
+  { "Slack",    NULL,       NULL,       1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
@@ -60,6 +62,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *logoutcmd[] = { "dmenu-poweroff", NULL };
 
 static Key keys[] = {
   /* modifier                     key        function        argument */
@@ -96,6 +99,7 @@ static Key keys[] = {
   TAGKEYS(                        XK_8,                      7)
   TAGKEYS(                        XK_9,                      8)
   { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = logoutcmd } },
 };
 
 /* button definitions */
